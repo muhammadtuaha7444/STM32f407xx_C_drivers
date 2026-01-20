fbfdf44052483a0630a376eddedd2e43be447d7f
MuhammadTuaha744 <tahajatt638@gmail.com>
2026-01-20 22:28:19 +0500

    feat: SPI Init API added

--- drivers/Inc/stm32f407xx_driver_spi.h
+++ drivers/Inc/stm32f407xx_driver_spi.h
@@ -25,21 +25,98 @@
 
 typedef struct 
 {
-   uint8_t SPI_DeviceMode;
-   uint8_t SPI_BusConfig;
-   uint8_t SPI_DFF;
-   uint8_t SPI_CPHA;
-   uint8_t SPI_CPOL;
-   uint8_t SPI_SSM;
-   uint8_t SPI_Speed;
+   uint8_t SPI_DeviceMode;      /* !<Defined @SPI_DeviceMode>   */
+   uint8_t SPI_BusConfig;       /* !<Defined @SPI_Bus_Config>   */
+   uint8_t SPI_DFF;             /* !<Defined @SPI_DFF>   */
+   uint8_t SPI_CPHA;            /* !<Defined @SPI_CPHA>   */
+   uint8_t SPI_CPOL;            /* !<Defined @SPI_CPOL>   */
+   uint8_t SPI_SSM;             /* !<Defined @SPI_SSM>   */
+   uint8_t SPI_Speed;           /* !<Defined @SPI_Speed>   */    
 }SPI_Config_t;
 
 typedef struct 
 {
     SPI_RegDef_t *pSPI_reg;
-    SPI_Config_t pSPI_config;
+    SPI_Config_t SPI_config;
 }SPI_Handler_t;
 
+
+/*
+ * SPI MACRO definations
+ * @brief: Reffer the to confgiure the SPI
+ * 1. SPI Control Register 1 Bit Defination Values
+ * 2. SPI Control Register 1 Bit Place Definations
+ */
+
+
+/* 1. SPI Control Register 1 Bit Defination Values */
+/*
+*@SPI_DeviceMode
+*/
+#define SPI_SLAVE_CONFIG        0           //0: Slave configuration
+#define SPI_MASTER_CONFIG       1           //1: Master configuration
+
+/*
+*@SPI_Bus_Config
+*/
+#define SPI_F_DUPLEX_EN           0         //0: 2-line unidirectional data mode selected
+#define SPI_H_DUPLEX_EN           1         //1: 1-line bidirectional data mode selected
+#define SPI_SIMPLEX_TX_EN         3         //3: 1-line simplex mode enabled
+
+/*
+*@SPI_DFF
+*/
+#define SPI_8_BIT_EN              0         //0: 8-bit data frame format transmission/reception
+#define SPI_16_BIT_E1             1         //1: 16-bit data frame format transmission/reception
+
+/*
+*@SPI_CPHA
+*/
+#define SPI_CPHA_HIGH             0         //0: CK to 0 when idle
+#define SPI_CPHA_LOW              1         //1: CK to 1 when idle
+
+
+/*
+*@SPI_CPOL
+*/
+#define SPI_CPOL_HIGH             0         //0: The first clock transition is the first data capture edge
+#define SPI_CPOL_LOW              1         //1: The second clock transition is the first data capture edge
+
+
+/*
+*@SPI_SSM
+*/
+#define SPI_SSM_DIS               0         //0: Software slave management disabled
+#define SPI_SSM_EN                1         //1: Software slave management enabled
+
+
+/*
+*@SPI_Speed
+*/
+#define SPI_BAUD_RATE_2           0         //0000: fPCLK/2
+#define SPI_BAUD_RATE_4           1         //0001: fPCLK/4
+#define SPI_BAUD_RATE_8           2         //0010: fPCLK/8
+#define SPI_BAUD_RATE_16          3         //0011: fPCLK/16
+#define SPI_BAUD_RATE_32          4         //0100: fPCLK/32
+#define SPI_BAUD_RATE_64          5         //0101: fPCLK/64
+#define SPI_BAUD_RATE_128         6         //0110: fPCLK/128
+#define SPI_BAUD_RATE_256         7         //0111: fPCLK/256
+
+/* 2. SPI Control Register 1 Bit Place Defination */
+#define SPI_BIDI_MODE_BIT         15
+#define SPI_BIDI_OE_BIT           14
+#define SPI_CRC_EN_BIT            13
+#define CRC_NEXT_BIT              12
+#define DFF_BIT                   11
+#define RX_ONLY_BIT               10
+#define SSM_BIT                   9
+#define SSI_BIT                   8
+#define LSB_FIRST_BIT             7
+#define SPE_BIT                   6
+#define SPE_BR_BIT                3
+#define MSTR_BIT                  2
+#define CPOL_BIT                  1
+#define CPHA_BIT                  0
 
 /********************************************************************************
  * 						GPIO APIs Decarations are written here
--- drivers/Src/stm32f407xx_driver_spi.c
+++ drivers/Src/stm32f407xx_driver_spi.c
@@ -63,7 +63,38 @@ void SPI_ClockCtrl(SPI_RegDef_t* pSPIx, uint8_t EnDis)
  */
 void SPI_Init(SPI_Handler_t *pSPIHandler)
 {
+    //1. Configure the SPI Device Mode
+    uint32_t temp = 0; 
+    temp|= (pSPIHandler->SPI_config.SPI_DeviceMode) << (MSTR_BIT);
+    
 
+    //2. Configure the Bus Speed
+    if(pSPIHandler->SPI_config.SPI_BusConfig == SPI_F_DUPLEX_EN)
+    {
+        temp&= ~(1 << SPI_BIDI_MODE_BIT);
+    }else if(pSPIHandler->SPI_config.SPI_BusConfig == SPI_H_DUPLEX_EN)
+    {
+        temp|= (1 << SPI_BIDI_OE_BIT);
+    }else if(pSPIHandler->pSPI_reg->SPI_CR1 == SPI_SIMPLEX_TX_EN)
+    {
+
+        temp|= (1 << SPI_BIDI_OE_BIT);
+        pSPIHandler->pSPI_reg->SPI_CR1 |=(temp);
+    }
+
+    //3. Configure the Baud Rate
+    temp|= (pSPIHandler->SPI_config.SPI_Speed) << (SPE_BR_BIT);
+    //4. Confgiure the SPI data frame
+    temp|= (pSPIHandler->SPI_config.SPI_DFF)   << (DFF_BIT);
+    //5. Configure the CPHA 
+    temp|= (pSPIHandler->SPI_config.SPI_CPHA)  << (DFF_BIT);
+    //6. Configure the CPOL
+    temp|= (pSPIHandler->SPI_config.SPI_CPOL)  << (CPOL_BIT);
+    //7. COnfigure the SSM
+    temp|= (pSPIHandler->SPI_config.SPI_SSM)   << (SSM_BIT);
+
+    pSPIHandler->pSPI_reg->SPI_CR1 = temp;
+    
 }
 
 /*******************************************************************************
@@ -74,6 +105,19 @@ void SPI_Init(SPI_Handler_t *pSPIHandler)
  */
 void SPI_DeInit(SPI_RegDef_t *pSPIx)
 {
+    if(SPI1)
+    {
+        RCC->APB2RSTR|= (1 << 12);
+    }else if(SPI2)
+    {
+        RCC->APB1RSTR|= (1 << 14);
+    }else if(SPI3)
+    {
+        RCC->APB1RSTR|= (1 << 15);
+    }else if(SPI4)
+    {
+        RCC->APB2RSTR|= (1 << 13);
+    }
 
 }
 
